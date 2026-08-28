d03e66a3eb1d9f041ba2adfd8eebf3ede17700e0
mycitago <mycitago@gmail.com>
2026-08-28 14:56:41 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 window.CITAS_CONFIG = Object.freeze({
 
   // 1) Supabase > Project Settings > API > Project URL
-  supabaseUrl: 'https://rienqmmrxzuseiaweugz.supabase.co/rest/v1/',
+  supabaseUrl: 'https://rienqmmrxzuseiaweugz.supabase.co',
 
   // 2) Supabase > Project Settings > API > Publishable key / anon key
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZW5xbW1yeHp1c2VpYXdldWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MzE1OTUsImV4cCI6MjEwMzUwNzU5NX0.DYcuR-LBlb_nay9aU-99N88PdbPeRFJZsU_J9gaocbE',
